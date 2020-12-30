fd58f136afa8106609219b8d073a245ac6729efc
Avijit Karmaker <ajkacca@gmail.com>
2020-12-30 12:49:47 +0200

    add tests for files

---
+++ src/tests/Domelements.test.js
@@ -0,0 +1,111 @@
+import Domelements from '../Domelements';
+
+
+test('Check existance of projectlist container', () => {
+  const Dom=Domelements();
+  expect(Dom.projectlist().classList.contains("projectlist")).toBe(true);
+});
+
+test('Check existance of ul tag in projectlist', () => {
+  const Dom=Domelements();
+  expect(Dom.projectlist().tagName).toBe("UL");
+});
+
+
+test('Check existance of projectform element', () => {
+  const Dom=Domelements();
+  expect(Dom.projectform().classList.contains("projectform")).toBe(true);
+});
+
+test('Check existance of form tag in project form', () => {
+  const Dom=Domelements();
+  expect(Dom.projectform().tagName).toBe("FORM");
+});
+
+test('Check existance of projectcont element', () => {
+  const Dom=Domelements();
+  expect(Dom.projectcont().classList.contains("projectbar")).toBe(true);
+});
+
+
+test('Check existance of div tag in project container', () => {
+  const Dom=Domelements();
+  expect(Dom.projectcont().tagName).toBe("DIV");
+});
+
+test('Check existance of taskheading element', () => {
+  const Dom=Domelements();
+  expect(Dom.taskheading().classList.contains("theading")).toBe(true);
+});
+
+test('Check existance of div tag in taskheading container', () => {
+  const Dom=Domelements();
+  expect(Dom.taskheading().tagName).toBe("DIV");
+});
+
+test('Check existance of tasklist container', () => {
+  const Dom=Domelements();
+  expect(Dom.tasklist().classList.contains("tasklist")).toBe(true);
+});
+
+test('Check existance of ul tag in tasklist', () => {
+  const Dom=Domelements();
+  expect(Dom.tasklist().tagName).toBe("UL");
+});
+
+test('Check existance of taskform element', () => {
+  const Dom=Domelements();
+  expect(Dom.taskform().classList.contains("taskform")).toBe(true);
+});
+
+test('Check if the taskform is hidden', () => {
+  const Dom=Domelements();
+  expect(Dom.taskform().classList.contains("hide")).toBe(true);
+});
+
+test('Check existance of form tag in taskform', () => {
+  const Dom=Domelements();
+  expect(Dom.taskform().tagName).toBe("FORM");
+});
+
+test('Check existance of taskupdateform element', () => {
+  const Dom=Domelements();
+  expect(Dom.taskupdateform().classList.contains("taskupdate")).toBe(true);
+});
+
+test('Check if the taskupdateform is also a taskform', () => {
+  const Dom=Domelements();
+  expect(Dom.taskupdateform().classList.contains("taskform")).toBe(true);
+});
+
+test('Check existance of form tag in taskupdateform', () => {
+  const Dom=Domelements();
+  expect(Dom.taskupdateform().tagName).toBe("FORM");
+});
+
+
+test('Check existance of taskinfo element', () => {
+  const Dom=Domelements();
+  expect(Dom.taskinfo().classList.contains("taskinfo")).toBe(true);
+});
+
+test('Check if the taskupdateform is also a taskform', () => {
+  const Dom=Domelements();
+  expect(Dom.taskupdateform().classList.contains("taskform")).toBe(true);
+});
+
+test('Check existance of div tag in taskupdateform', () => {
+  const Dom=Domelements();
+  expect(Dom.taskinfo().tagName).toBe("DIV");
+});
+
+
+test('Check existance of taskcontainer element', () => {
+  const Dom=Domelements();
+  expect(Dom.taskcont().classList.contains("taskbar")).toBe(true);
+});
+
+test('Check existance of div tag in taskcontainer', () => {
+  const Dom=Domelements();
+  expect(Dom.taskcont().tagName).toBe("DIV");
+});
---
+++ src/tests/Taskgenerator.test.js
@@ -0,0 +1,22 @@
+import Taskgenertor from '../Taskgenerator';
+
+
+test('create todo object and check todo name', () => {
+  const todo = Taskgenertor('Todo 1');
+  expect(todo.name).toBe('Todo 1');
+});
+
+test('create todo object and check todo date', () => {
+  const todo = Taskgenertor("Todo 1",'15/01/20');
+  expect(todo.date).toBe('15/01/20');
+});
+
+test('create todo object and check todo priority', () => {
+  const todo = Taskgenertor("Todo 1",'15/01/20',"high");
+  expect(todo.priority).toBe('high');
+});
+
+test('create todo object and check todo description', () => {
+  const todo = Taskgenertor("Todo 1",'15/01/20',"high","Have to make dinner");
+  expect(todo.description).toBe("Have to make dinner");
+});
